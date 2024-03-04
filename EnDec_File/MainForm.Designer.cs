74b3d3dc9b71ac133a1147f64d2f907e07296c59
madmozell <Bryla.K.S@nmu.one>
2024-03-04 18:18:56 +0200

    Changes №1 (New project C#): 18:18
    1. An empty C# project has been created.
    2. The file was created: ".gitattributes".
    3. File created: ".gitignore".
    4. A file has been created: “ReadMe.md” - in which the changes to this project will be structurally described in text format.
    5. The element “Assembly information...” has been created and configured.
    6. The element “Windows Forms” has been created and configured.
    7. Added file: "icon.ico" - in the form of a shortcut to the executable (.exe) file.

---
+++ EnDec_File/MainForm.Designer.cs
@@ -0,0 +1,56 @@
+﻿namespace EnDec_File
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(738, 374);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(754, 413);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "EnDec File";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ EnDec_File/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General assembly information is managed using the following
+// set of attributes. Change the values of these attributes to change the information
+// general information about this assembly.
+[assembly: AssemblyTitle("EnDec File")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EnDec Product")]
+[assembly: AssemblyCopyright("Bryla Karina, group 125m-23-1")]
+[assembly: AssemblyTrademark("Dnipro University of Technology")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible set to FALSE makes types in the assembly invisible
+// for COM components. If you need to access a type in this assembly via
+// COM, set the ComVisible attribute to TRUE for this type.
+[assembly: ComVisible(false)]
+
+// The following GUID serves to identify the type library if this project is to be visible to COM
+[assembly: Guid("5c6c31cb-4957-40c9-9d3e-f47c8aa3132a")]
+
+// Assembly version information consists of the following four values:
+//
+//  Major version number
+//  Additional version number
+//  Build number
+//  Editorial
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
